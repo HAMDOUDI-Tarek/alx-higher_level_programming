ecc3422b6c15fb61ad46d5dc8f851a14dcf96dff
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-11-27 11:56:01 -0800

    all

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * check_cycle - loop check.
+ * @list: head.
+ * Return: 1 or 0.
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
+
+	if (!list)
+	{
+		return (0);
+	}
+	slow = list;
+	fast = list->next;
+	while (fast && slow && fast->next)
+	{
+		if (slow == fast)
+		{
+			return (1);
+		}
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (0);
+}
