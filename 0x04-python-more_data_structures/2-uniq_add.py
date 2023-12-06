024f164637717bbb901f01a2d0ae9cbed315d3d7
HAMDOUDI-Tarek <hamdouditarek@gmail.com>
2023-12-06 10:19:09 -0800

    all

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
+    """
+    Function that adds all unique
+    integers in a list (only once for each integer)
+    """
+    return len([num if num not in new_list for num in my_list])
